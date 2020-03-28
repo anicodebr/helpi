a232926aaae9e1ffade40b72be9bc1969fa30b3c
Felipe Correa <pecolps@outlook.com>
2020-03-28 18:04:48 -0300

    Change env on database.js

--- backend/src/config/database.js
+++ backend/src/config/database.js
@@ -1,7 +1,8 @@
+require('dotenv').config();
 module.exports = {
   "username": process.env.DB_USERNAME || 'root',
   "password": process.env.DB_PASSWORD || null,
-  "database": process.env.DBE_NAME     || 'teste',
+  "database": process.env.DB_NAME     || 'teste',
   "host":     process.env.DB_HOSTNAME || "127.0.0.1",
   "dialect":  "mysql"
 }
