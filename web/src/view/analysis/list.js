b76eb6e0e0fceda824c4da39ff485aff848b37d6
gmartinu <53321310+gmartinu@users.noreply.github.com>
2020-03-28 00:09:51 -0300

    Layout and User Pending List done

--- web/src/view/analysis/list.js
+++ web/src/view/analysis/list.js
@@ -3,6 +3,8 @@ import React, { Component } from 'react';
 // import { history } from '../../redux/_helpers'
 import { withRouter } from 'react-router-dom'
 import { connect } from 'react-redux';
+import { Grid } from "@material-ui/core"
+import MaterialTable from 'material-table'
 
 class UsersList extends Component {
     // constructor(props) {
@@ -19,7 +21,56 @@ class UsersList extends Component {
     render() { 
         return (
             <>
-                <h1 onClick={() => {console.log(this.props)}}> User List </h1>
+                <Grid container direction="row" justify="center" alignItems="center">
+                    <Grid item xs={12} md={10} sm={12}>
+                        <MaterialTable
+                            columns={[
+                                { title: 'Nome', field: 'name' },
+                                { title: 'Sobre Nome', field: 'surname' },
+                                { title: 'Ano de Nascimento', field: 'birthYear', type: 'numeric' },
+                                { title: 'Cidade de nascimento', field: 'birthCity', lookup: { 34: 'İstanbul', 63: 'Şanlıurfa' } }
+                            ]}
+                            options={{
+                                toolbar: false,
+                                pageSize: 8
+                            }}
+                            onRowClick={((evt, selectedRow) => console.log(selectedRow))}
+                            data={[
+                                { name: 'Mehmet', surname: 'Baran', birthYear: 1987, birthCity: 63 },
+                                { name: 'Mehmet', surname: 'Baran', birthYear: 1987, birthCity: 63 },
+                                { name: 'Mehmet', surname: 'Baran', birthYear: 1987, birthCity: 63 },
+                                { name: 'Mehmet', surname: 'Baran', birthYear: 1987, birthCity: 63 },
+                                { name: 'Mehmet', surname: 'Baran', birthYear: 1987, birthCity: 63 },
+                                { name: 'Mehmet', surname: 'Baran', birthYear: 1987, birthCity: 63 },
+                                { name: 'Mehmet', surname: 'Baran', birthYear: 1987, birthCity: 63 },
+                                { name: 'Mehmet', surname: 'Baran', birthYear: 1987, birthCity: 63 },
+                                { name: 'Mehmet', surname: 'Baran', birthYear: 1987, birthCity: 63 },
+                                { name: 'Mehmet', surname: 'Baran', birthYear: 1987, birthCity: 63 },
+                                { name: 'Mehmet', surname: 'Baran', birthYear: 1987, birthCity: 63 },
+                                { name: 'Mehmet', surname: 'Baran', birthYear: 1987, birthCity: 63 },
+                                { name: 'Mehmet', surname: 'Baran', birthYear: 1987, birthCity: 63 },
+                                { name: 'Mehmet', surname: 'Baran', birthYear: 1987, birthCity: 63 },
+                                { name: 'Mehmet', surname: 'Baran', birthYear: 1987, birthCity: 63 },
+                                { name: 'Mehmet', surname: 'Baran', birthYear: 1987, birthCity: 63 },
+                                { name: 'Mehmet', surname: 'Baran', birthYear: 1987, birthCity: 63 },
+                                { name: 'Mehmet', surname: 'Baran', birthYear: 1987, birthCity: 63 },
+                                { name: 'Mehmet', surname: 'Baran', birthYear: 1987, birthCity: 63 },
+                                { name: 'Mehmet', surname: 'Baran', birthYear: 1987, birthCity: 63 },
+                                { name: 'Mehmet', surname: 'Baran', birthYear: 1987, birthCity: 63 },
+                                { name: 'Mehmet', surname: 'Baran', birthYear: 1987, birthCity: 63 },
+                                { name: 'Mehmet', surname: 'Baran', birthYear: 1987, birthCity: 63 },
+                                { name: 'Mehmet', surname: 'Baran', birthYear: 1987, birthCity: 63 },
+                                { name: 'Mehmet', surname: 'Baran', birthYear: 1987, birthCity: 63 },
+                                { name: 'Mehmet', surname: 'Baran', birthYear: 1987, birthCity: 63 },
+                                { name: 'Mehmet', surname: 'Baran', birthYear: 1987, birthCity: 63 },
+                                { name: 'Mehmet', surname: 'Baran', birthYear: 1987, birthCity: 63 },
+                                { name: 'Mehmet', surname: 'Baran', birthYear: 1987, birthCity: 63 },
+                                { name: 'Mehmet', surname: 'Baran', birthYear: 1987, birthCity: 63 },
+                                { name: 'Mehmet', surname: 'Baran', birthYear: 1987, birthCity: 63 }
+                            ]}
+                            />
+                    </Grid>
+                </Grid>
             </>
         );
     }
