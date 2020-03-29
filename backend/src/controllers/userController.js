3e73e61f362296a8d6c4296352a4057f1274297f
gmartinu <53321310+gmartinu@users.noreply.github.com>
2020-03-28 23:54:44 -0300

    Update faltando atualizar, store completo.

--- backend/src/controllers/userController.js
+++ backend/src/controllers/userController.js
@@ -1,4 +1,4 @@
-const { User } = require('../models');
+const { User, Entregador, Cliente, Endereco } = require('../models');
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 
@@ -34,7 +34,10 @@ module.exports = {
         })
     },
     async show(req,res){
-        User.findByPk(req.params.id, { attributes: [ 'id', 'name', 'email', "dt_nasc", "cpf", "tel", 'createdAt']})
+        User.findByPk(req.params.id, { 
+            attributes: [ 'id', 'name', 'email', "dt_nasc", "cpf", "tel", 'foto','createdAt'],
+            include: [Entregador, Cliente]
+        })
         .then(user => {
             user?
             res.status(200).json(user):
@@ -44,15 +47,49 @@ module.exports = {
     async store(req,res){
         let password = await bcrypt.hash(req.body.password,10);
         User.findOrCreate({where: {email: req.body.email}, defaults: {
-            password: password, name: req.body.name, dt_nasc: req.body.dt_nasc, cpf: req.body.cpf, tel: req.body.tel
+            password: password, name: req.body.name, dt_nasc: new Date(req.body.dt_nasc), 
+            cpf: req.body.cpf, tel: req.body.tel, foto: req.body.foto
         }})
         .then(([user, created]) => {
-            console.log(user.get({
-                plain: true
-            }))
-            created?
-            res.status(200).json(null):
-            res.status(401).json(null)
+            let _user = user.get({plain: true})
+            if(created){
+                if(req.body.cliente){
+                    Endereco.create({
+                        endereco: req.body.endereco, numero: req.body.numero, cep: req.body.cep,
+                        referencia: req.body.referencia, complemento: req.body.complemento,
+                        uf: req.body.uf, bairro: req.body.bairro, cidade: req.body.cidade, pais: req.body.pais
+                    }).then(async (endereco) => {
+                        let _endereco = endereco.get({plain: true});
+                        let _cliente = await Cliente.create({ EnderecoId: _endereco.id});
+                        User.findOne({where: {id: _user.id}}, {attributes: ['ClienteId']})
+                        .then(async () => {
+                            user.ClienteId = _cliente.dataValues.id;
+                            await user.save().then(() => {
+                                res.status(200).json(null)
+                            })
+                        })
+                    }).catch((err) => {
+                        console.log(err)
+                        res.status(500).json(null)
+                    })
+                }else{
+                    Entregador.create({
+                        rg_frente: req.body.rg_frente, rg_tras: req.body.rg_tras, descricao: req.body.descricao
+                    }).then(async (entregador) => {
+                        let _entregador = entregador.get({plain: true});
+                        User.findOne({where: {id: _user.id}}, {attributes: ['EntregadorId']})
+                        .then(async () => {
+                            user.EntregadorId = _entregador.id;
+                            await user.save().then(() => {
+                                res.status(200).json(null)
+                            })
+                        })
+                    })
+                }
+            }else{ res.status(401).json(null) }
+        }).catch((err) => {
+            console.log(err)
+            res.status(500).json(null)
         })
     },
     async update(req,res){
