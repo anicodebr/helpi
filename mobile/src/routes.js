06d69d21993b765d669fa58da606366cb92d1ce0
Felipe Correa <pecolps@outlook.com>
2020-04-03 21:12:21 -0300

    Refactoring routes and it's comments

--- mobile/src/routes.js
+++ mobile/src/routes.js
@@ -6,6 +6,12 @@ import Login1 from './pages/Login1';
 import Login2 from './pages/Login2';
 import Login3 from './pages/Login3';
 import CadastroUser1 from './pages/CadastroUser1';
+import CadastroUser2 from './pages/CadastroUser2';
+import CadastroUser3 from './pages/CadastroUser3';
+import CadastroEndereco1 from './pages/CadastroEndereco1';
+import CadastroEndereco2 from './pages/CadastroEndereco2';
+import CadastroEndereco3 from './pages/CadastroEndereco3';
+import CadastroEndereco4 from './pages/CadastroEndereco4';
 
 const Stack = createStackNavigator();
 
@@ -17,6 +23,8 @@ const generalScreenOptions = {
 
 //Separei em funções caso futuramente fique grande o
 //o suficiente para ter que dividir em arquivos
+
+// -------------------------------------- Rotas de Inicio ------------------------------------------
 function InicioRoutes(){
     return(
     <Stack.Navigator
@@ -46,6 +54,7 @@ function InicioRoutes(){
     )
 }
 
+// -------------------------------- Rotas de Cadastro de usuário -----------------------------------
 function CadastroUserRoutes(){
     return (
     <Stack.Navigator
@@ -56,18 +65,61 @@ function CadastroUserRoutes(){
             component={CadastroUser1}
             options={{ title: 'Cadastro - Processo de Usuário 1' }}
         /> 
+        <Stack.Screen
+            name="CadastroUser2"
+            component={CadastroUser2}
+            options={{ title: 'Cadastro - Processo de Usuário 2' }}
+        /> 
+        <Stack.Screen
+            name="CadastroUser3"
+            component={CadastroUser3}
+            options={{ title: 'Cadastro - Processo de Usuário 2' }}
+        /> 
     </Stack.Navigator>
     )
 }
 
+// -------------------------------- Rotas de Cadastro de Endereço -----------------------------------
+function CadastroEnderecoRoutes(){
+    return (
+    <Stack.Navigator
+        initialRouteName="CadastroEndereco1"
+        screenOptions={{...generalScreenOptions}}
+    > 
+        <Stack.Screen
+            name="CadastroEndereco1"
+            component={CadastroEndereco1}
+            options={{ title: 'Cadastro - Processo de Endereço 1' }}
+        />
+        <Stack.Screen
+            name="CadastroEndereco2"
+            component={CadastroEndereco2}
+            options={{ title: 'Cadastro - Processo de Endereço 2' }}
+        />
+        <Stack.Screen
+            name="CadastroEndereco3"
+            component={CadastroEndereco3}
+            options={{ title: 'Cadastro - Processo de Endereço 3' }}
+        />
+        <Stack.Screen
+            name="CadastroEndereco4"
+            component={CadastroEndereco4}
+            options={{ title: 'Cadastro - Processo de Endereço 4' }}
+        />
+    </Stack.Navigator>
+    )
+}
+
+// -------------------------------------- Main Router Render ----------------------------------------
 function Routes(){
     return(
         <NavigationContainer>
             <Stack.Navigator 
-            initialRouteName="CadastroUserNavigator"
+            initialRouteName="InicioNavigator"
             screenOptions={{...generalScreenOptions}}>
                 <Stack.Screen name="InicioNavigator"        component={InicioRoutes} />
                 <Stack.Screen name="CadastroUserNavigator"  component={CadastroUserRoutes} />
+                <Stack.Screen name="CadastroEnderecoRoutes"  component={CadastroEnderecoRoutes} />
             </Stack.Navigator>
         </NavigationContainer>
         );
